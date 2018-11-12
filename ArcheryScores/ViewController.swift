dbd10e89185ee1ec7125cdc286ebd090acccb982
Aamina Munzir <aaminamunzir@aaminas-macbook-air.local>
2018-11-12 12:25:43 -0600

    finished text field outlets

--- ArcheryScores/ViewController.swift
+++ ArcheryScores/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var roundOneArrowOneTF: UITextField!
+    @IBOutlet weak var roundOneArrowTwoTF: UITextField!
+    @IBOutlet weak var roundOneArrowThreeTF: UITextField!
+    
+    @IBOutlet weak var roundTwoArrowOneTF: UITextField!
+    @IBOutlet weak var roundTwoArrowTwoTF: UITextField!
+    @IBOutlet weak var roundTwoArrowThreeTF: UITextField!
+    
+    @IBOutlet weak var roundThreeArrowOne: UITextField!
+    @IBOutlet weak var roundThreeArrowTwoTF: UITextField!
+    @IBOutlet weak var roundThreeArrowThreeTF: UITextField!
+    
+    @IBOutlet weak var roundFourArrowOneTF: UITextField!
+    @IBOutlet weak var roundFourArrowTwoTF: UITextField!
+    @IBOutlet weak var roundFourArrowThreeTF: UITextField!
+    
+    @IBOutlet weak var roundFiveArrowOneTF: UITextField!
+    @IBOutlet weak var roundFiveArrowTwoTF: UITextField!
+    @IBOutlet weak var roundFiveArrowThreeTF: UITextField!
+    
+    @IBOutlet weak var roundSixArrowOneTF: UITextField!
+    @IBOutlet weak var roundSixArrowTwoTF: UITextField!
+    @IBOutlet weak var roundSixArrowThreeTF: UITextField!
+    
+    @IBOutlet weak var roundSevenArrowOneTF: UITextField!
+    @IBOutlet weak var roundSevenArrowTwoTF: UITextField!
+    @IBOutlet weak var roundSevenArrowThreeTF: UITextField!
+    
+    @IBOutlet weak var roundEightArrowOneTF: UITextField!
+    @IBOutlet weak var roundEightArrowTwoTF: UITextField!
+    @IBOutlet weak var roundEightArrowThreeTF: UITextField!
+    
+    @IBOutlet weak var roundNineArrowOneTF: UITextField!
+    @IBOutlet weak var roundNineArrowTwoTF: UITextField!
+    @IBOutlet weak var roundNineArrowThreeTF: UITextField!
+    
+    @IBOutlet weak var roundTenArrowOneTF: UITextField!
+    @IBOutlet weak var roundTenArrowTwoTF: UITextField!
+    @IBOutlet weak var roundTenArrowThreeTF: UITextField!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    
 
 }
 
