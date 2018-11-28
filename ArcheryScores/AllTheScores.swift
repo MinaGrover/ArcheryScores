99cef9483e4230105f8a00b81f335ea1323a4f21
Aamina Munzir <aaminamunzir@aaminas-macbook-air.local>
2018-11-28 11:39:18 -0600

    added AllTheScores class

---
+++ ArcheryScores/AllTheScores.swift
@@ -0,0 +1,26 @@
+//
+//  AllTheScores.swift
+//  ArcheryScores
+//
+//  Created by Aamina Munzir on 11/28/18.
+//  Copyright © 2018 Aamina Munzir. All rights reserved.
+//
+
+import UIKit
+
+class AllTheScores: Codable {
+    
+    var roundOneScores = [0, 0, 0, 0, 0]
+    var roundTwoScores = [0, 0, 0, 0, 0]
+    var roundThreeScores = [0, 0, 0, 0, 0]
+    var roundFourScores = [0, 0, 0, 0, 0]
+    var roundFiveScores = [0, 0, 0, 0, 0]
+    var roundSixScores = [0, 0, 0, 0, 0]
+    var roundSevenScores = [0, 0, 0, 0, 0]
+    var roundEightScores = [0, 0, 0, 0, 0]
+    var roundNineScores = [0, 0, 0, 0, 0]
+    var roundTenScores = [0, 0, 0, 0, 0]
+    
+    
+    
+}
